418fe0bea3da44fe063e6379dbf9a9c9d461341f
muhammad arindra khoiru syawal <MAK00006935@trac.astra.co.id>
2023-01-06 21:07:58 +0700

    feat: comment and variable

---
+++ variableAndComment.js
@@ -0,0 +1,31 @@
+//  This is Comment Single Line
+
+/*
+    This is comment multiple line
+    comment used for explaination code 
+    or make a documentation
+*/
+
+// Variable
+/*
+    Since EcmaScript6 variable at JS 
+    declare with let or const and not using
+    var anymore, because var can cause bug code
+*/ 
+let firstName;
+firstName = "Muhammad Arindra";
+
+console.log(firstName)
+
+// variable let can be declare more than one declaration
+firstName = "Arindra";
+console.log(firstName)
+
+// variable const can be declare only one 
+const lastName = "Khoiru Syawal";
+
+// if we declare const variable more than one it can be error
+lastName = "Syawal";
+console.log(lastName)
+
+
