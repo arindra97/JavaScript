6081714d1c3d0fa8925a8276a3ea5fa457d689db
muhammad arindra khoiru syawal <MAK00006935@trac.astra.co.id>
2023-02-10 13:58:55 +0700

    feat: Functional Programming

--- functional.js
+++ functional.js
@@ -37,3 +37,66 @@ console.log(newArray);
  * [ 'Harry!', 'Ron!', 'Jeff!', 'Thomas!' ]
  * 
  */
+
+// Array Filter
+const students = [
+  {
+    name: 'Harry',
+    score: 60,
+  },
+  {
+    name: 'James',
+    score: 88,
+  },
+  {
+    name: 'Ron',
+    score: 90,
+  },
+  {
+    name: 'Bethy',
+    score: 75,
+  }
+];
+
+const eligibleForScholarshipStudents = students.filter((student) => student.score > 85);
+
+console.log(eligibleForScholarshipStudents);
+
+/**
+ * output:
+ * [ { name: 'James', score: 88 }, { name: 'Ron', score: 90 } ]
+ * 
+ */
+ 
+const totalScore = students.reduce((acc, student) => acc + student.score, 0);
+
+console.log(totalScore);
+
+/**
+ * output:
+ * 313
+ * 
+ */
+
+// Array Sort
+const months = ['March', 'Jan', 'Feb', 'Dec'];
+months.sort();
+console.log(months);
+// output: [ 'Dec', 'Feb', 'Jan', 'March' ]
+
+const array1 = [1, 30, 4, 1000, 101, 121];
+array1.sort();
+console.log(array1);
+// output: [ 1, 1000, 101, 121, 30, 4 ]
+
+
+const compareNumber = (a, b) => {
+  return a - b;
+};
+const sorting = array1.sort(compareNumber);
+console.log(sorting);
+
+/**
+output
+[ 1, 4, 30, 1000 ]
+**/
